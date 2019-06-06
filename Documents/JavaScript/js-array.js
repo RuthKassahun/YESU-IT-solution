6d9c45315c64842e11b6b890a8f2416b5c15e2ab
Ruth Kassahun <ruthkehali@gmail.com>
2019-06-06 12:57:34 +0300

    add js array

---
+++ Documents/JavaScript/js-array.js
@@ -0,0 +1,18 @@
+var book = {
+  title : "The Diary of a wimpy kid"
+  author : "Jeff"
+  releaseDate : "2014"
+}
+
+console.log (book)
+//console.log (book.title)
+console.log (book[title])
+
+var food = ["pasta","bread"]
+
+console.log(food)
+
+for(var i=0; i< food.length ; i++)
+  {
+    console.log (food[i])
+  }
